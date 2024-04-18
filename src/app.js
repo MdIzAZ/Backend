f9dcbe4bf22704cda5ad0a3c665b2604671c2b97
SK MD IZAZ <skmdizaz1@gmail.com>
2024-04-18 10:54:20 +0530

    test

--- src/app.js
+++ src/app.js
@@ -20,5 +20,7 @@ import userRouter from "./routes/user.routes.js"
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
-
+app.get("/", (req, res)=>{
+  res.send(`<h1>hello Backend<h1>`)
+})
 export { app };
